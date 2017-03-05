c0ac59b8330f8d6a2e562e8e67f34a88e3db29e7
David Mark <david.ike.mark@gmail.com>
2017-03-05 14:02:23 +0000

    missing

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,23 @@
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import spark.ModelAndView;
 import spark.template.freemarker.FreeMarkerEngine;
 
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.Dictionary;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static spark.Spark.*;
@@ -17,39 +26,83 @@ public class Main {
 
   public static void main(String[] args) {
     String port = System.getenv("PORT");
-    if(port == null)
-      port = "9000";
+    if (port == null)
+      port = "9002";
 
     port(Integer.valueOf(port));
     staticFileLocation("/public");
 
-    String[] ingredients = { "Eggs", "Milk", "Rice", "White Rice", "Brown Rice", "Okra", "Tomatoes" };
+    String[] ingredients = {"Eggs", "Milk", "Rice", "White Rice", "Brown Rice", "Okra", "Tomatoes"};
 
     get("/ingredient", (req, res) -> {
       ArrayList<Ingredient> results = new ArrayList<Ingredient>();
       String searchIngredient = req.queryParams("ingredient");
-      for (String ingred: ingredients) {
-        if(ingred.toLowerCase().contains(searchIngredient.toLowerCase())){
+      for (String ingred : ingredients) {
+        if (ingred.toLowerCase().contains(searchIngredient.toLowerCase())) {
           results.add(new Ingredient(ingred, "images/rice.jpg"));
         }
       }
       return results;
     }, new JsonTransformer());
 
-//    get("/food", (req, res) -> {
-//      String ingredients = req.queryParams("ingredients");
+    get("/food", (req, res) -> {
+       List<Reciepe> results = new ArrayList<Reciepe>();
+      String request = req.queryParams("ingredients");
+
+      try {
+        String foodApiReq = "https://api.edamam.com/search?q=" + request
+                + "&app_id=7a66e8c5&app_key=8da9f0e829bb3d2cac3ac07e29f88b39&from=0&to=3&calories=gte%20591,%20lte%20722&health=alcohol-free";
+        Response data = makeRestCall(foodApiReq);
+
+
+        results = parseJson(data.readEntity(String.class));
+      }catch(Exception e){
+
+      }
+      return results;
+    }, new JsonTransformer());
+  }
+
+
+    public static Response makeRestCall(String url) throws Exception {
+      Client client = ClientBuilder.newClient();
+
+      WebTarget resource = client.target(url);
+
+      Invocation.Builder request = resource.request();
+      request.accept(MediaType.APPLICATION_JSON);
+
+      Response response = request.get();
+      return response;
+    }
+
+    public static List<Reciepe> parseJson(String json) throws Exception {
+      List<Reciepe> reciepes = new ArrayList<Reciepe>();
+
+      ObjectMapper objectMapper = new ObjectMapper();
+      JsonNode root = objectMapper.readTree(json);
+      JsonNode results = root.at("/hits");
+      for(JsonNode reciepeNode: results){
+        List<String> steps = new ArrayList<>();
+        String reciepeName = "";
+        JsonNode reqIngredientsNode = reciepeNode.at("/recipe/ingredientLines");
+        JsonNode nameNode = reciepeNode.at("/recipe/label");
+        JsonNode imageNode = reciepeNode.at("/recipe/image");
+        for (final JsonNode step : reqIngredientsNode) {
+          steps.add(step.asText());
+        }
+        reciepes.add(new Reciepe(nameNode.asText(), imageNode.asText(), steps));
+      }
+
+      return reciepes;
+    }
+
+//    get("/", (request, response) -> {
+//        Map<String, Object> attributes = new HashMap<>();
+//        attributes.put("message", "Hello World!");
 //
-//
-//      return "";
-//    });
-
-
-    get("/", (request, response) -> {
-        Map<String, Object> attributes = new HashMap<>();
-        attributes.put("message", "Hello World!");
-
-        return new ModelAndView(attributes, "index.ftl");
-    }, new FreeMarkerEngine());
+//        return new ModelAndView(attributes, "index.ftl");
+//    }, new FreeMarkerEngine());
 
     //HikariConfig config = new  HikariConfig();
     //config.setJdbcUrl(System.getenv("JDBC_DATABASE_URL"));
@@ -79,6 +132,5 @@ public class Main {
 
 
 
-  }
 
 }
--- src/main/java/Reciepe.java
+++ src/main/java/Reciepe.java
@@ -1,7 +1,30 @@
-package PACKAGE_NAME;
+import java.util.List;
 
 /**
  * Created by David on 04/03/2017.
  */
 public class Reciepe {
+    private final String name;
+    private final String image;
+    private final List<String> steps;
+
+    public Reciepe(String name, String image, List<String> steps) {
+
+        this.name = name;
+        this.image = image;
+        this.steps = steps;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public List<String> getSteps() {
+        return steps;
+    }
 }
+
